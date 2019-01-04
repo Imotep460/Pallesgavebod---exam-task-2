5089ffd50412a05553aae9afe7cc6655d4dee9bb
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-01-04 15:27:48 +0100

    ...

--- Pallesgavebod/Controllers/GiftsController.cs
+++ Pallesgavebod/Controllers/GiftsController.cs
@@ -4,45 +4,77 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pallesgavebod.Data;
+using Pallesgavebod.Models;
 
 namespace Pallesgavebod.Controllers
 {
     public class GiftsController : Controller
     {
-        // GET: Gifts
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public GiftsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        // GET: Gifts/Details/5
-        public ActionResult Details(int id)
+        // GET: Gifts
+        public ActionResult Gifts()
         {
-            return View();
+            var allGifts = _context.Gifts;
+
+            return View(allGifts);
         }
+
+        //// GET: Gifts/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
         // GET: Gifts/Create
-        public ActionResult Create()
+        public ActionResult Create(GiftItemDTO gift)
         {
-            return View();
+            var newitem = new Gift();
+
+            newitem.CreationDate = DateTime.Now;
+            newitem.Description = gift.Description;
+            newitem.Title = gift.Title;
+            newitem.BoyGift = gift.BoyGift;
+            newitem.GirlGift = gift.GirlGift;
+
+            _context.Gifts.Add(newitem);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult boys()
+        {
+            var boyitems = from a in _context.Gifts
+                           where a.BoyGift == true
+                           select new Gift { GiftNumber = a.GiftNumber, Title = a.Title, CreationDate = a.CreationDate, Description = a.Description };
+
+            return View(boyitems);
         }
 
         // POST: Gifts/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
         // GET: Gifts/Edit/5
         public ActionResult Edit(int id)
@@ -51,21 +83,21 @@ namespace Pallesgavebod.Controllers
         }
 
         // POST: Gifts/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
         // GET: Gifts/Delete/5
         public ActionResult Delete(int id)
@@ -74,20 +106,20 @@ namespace Pallesgavebod.Controllers
         }
 
         // POST: Gifts/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
---
+++ Pallesgavebod/Models/GiftItemDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pallesgavebod.Models
+{
+    public class GiftItemDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool BoyGift { get; set; }
+        public bool GirlGift { get; set; }
+    }
+}
