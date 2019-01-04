7e62c9814e3ceac011717dafdda57b0a0210c1b8
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-01-04 13:56:11 +0100

    added context

---
+++ Pallesgavebod/Controllers/GiftController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pallesgavebod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pallesgavebod.Controllers
+{
+    [Route("api/Gifts")]
+    [ApiController]
+    public class GiftController : ControllerBase
+    {
+        private readonly GiftsContext _context;
+
+        public GiftController(GiftsContext context)
+        {
+            _context = context;
+        }
+
+
+        //Get api/Gifts
+        [HttpGet]
+        public IEnumerable<Gift> GetGifts()
+        {
+            return _context.Gifts;
+        }
+
+        //POST api/<controller>
+        [HttpPost]
+        public IActionResult PostGift([FromBody] Gift gift)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Gifts.Add(gift);
+            _context.SaveChangesAsync();
+            return CreatedAtAction("GetGift", new { gift.GiftNumber }, gift);
+        }
+    }
+}
---
+++ Pallesgavebod/Models/GiftsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pallesgavebod.Models
+{
+    public class GiftsContext : DbContext
+    {
+        public DbSet<Gift> Gifts { get; set; }
+
+        //public DbSet<> { get; set; }
+
+        public GiftsContext(DbContextOptions<GiftsContext>options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
